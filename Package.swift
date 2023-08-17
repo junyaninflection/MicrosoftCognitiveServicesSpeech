0a1b38e4f9ec9c58a2fee89734fc95160a09a05d
Jun Yan <jun@inflection.ai>
2023-08-17 15:21:23 -0700

    rename

--- Package.swift
+++ Package.swift
@@ -8,18 +8,18 @@ let package = Package(
         .library(
             name: "MicrosoftCognitiveServicesSpeech",
             targets: [
-                "MicrosoftCognitiveServicesSpeech"
+                "MicrosoftCognitiveServicesSpeechTarget"
             ]),
     ],
     targets: [
         .target(
-            name: "MicrosoftCognitiveServicesSpeech",
+            name: "MicrosoftCognitiveServicesSpeechTarget",
             dependencies: [
-                .target(name: "MicrosoftCognitiveServicesSpeechBinary")
+                .target(name: "MicrosoftCognitiveServicesSpeech")
             ]
         ),
         .binaryTarget(
-            name: "MicrosoftCognitiveServicesSpeechBinary",
+            name: "MicrosoftCognitiveServicesSpeech",
             url: "https://csspeechstorage.blob.core.windows.net/drop/1.31.0/MicrosoftCognitiveServicesSpeech-XCFramework-1.31.0.zip",
             checksum: "4da55140eda8b1b3067baf0abbdc76f6238dd16a74dc4fc08edf328eb125ec81")
     ]
